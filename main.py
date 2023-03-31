5bc5e20f8e07bcd435f854f3b21108be1ee7cfb6
Aron <aronpjansen@gmail.com>
2023-03-31 11:42:53 +0200

    Implement download_repo_zip function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
 
 def download_repo_zip(url: str, target_path: str):
-    pass
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    with open(target_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+
 
 if __name__ == "__main__":
     print("GitHub Token Analyzer")
